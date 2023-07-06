15b00acfb904dc5918eb4d1a839fb78d676e874e
Nicollette Porca <20200108m.porca.nicollette.bscs@gmail.com>
2023-07-06 09:42:55 +0800

    index.html

---
+++ plugins/typed.js/typed.js
@@ -0,0 +1,20 @@
+var typed = new Typed('#element', {
+    strings: ['LOCATION', 'ROOMS', 'PLACES', 'DESTINATION'],
+    typeSpeed: 60,
+    backSpeed: 60,
+    loop: true,
+});
+
+var typed = new Typed('#element2', {
+    strings: ['LOCATION', 'ROOMS', 'PLACES', 'DESTINATION'],
+    typeSpeed: 60,
+    backSpeed: 60,
+    loop: true,
+});
+
+var typed = new Typed('#element3', {
+    strings: ['LOCATION', 'ROOMS', 'PLACES', 'DESTINATION'],
+    typeSpeed: 60,
+    backSpeed: 60,
+    loop: true,
+});
